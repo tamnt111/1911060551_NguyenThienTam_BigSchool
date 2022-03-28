001d03c967f793b19b10182c20a946ebbff663be
tamnt111 <101544249+tamnt111@users.noreply.github.com>
2022-03-28 12:36:38 +0700

    commit t28/03/2022 13:36

--- 1911060551_NguyenThienTam_BigSchool/Controllers/CoursesController.cs
+++ 1911060551_NguyenThienTam_BigSchool/Controllers/CoursesController.cs
@@ -79,6 +79,22 @@ namespace _1911060551_NguyenThienTam_BigSchool.Controllers
                 .ToList();
             return View(courses);
         } 
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Date = course.DateTime.ToString("dd/M/yyyy"),
+                Time = course.DateTime.ToString("HH:mm"),
+                Category = course.CategoryId,
+                Place = course.Place
+            };
+            return View("Create", viewModel);
+        }
 
 
     }
